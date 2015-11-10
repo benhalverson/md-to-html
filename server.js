e4cea21af1e3606c4ff4d20b479d0c553f7d8f45
Ben Halverson <benhalverson33@gmail.com>
2015-11-10 03:15:22 -0800

    update bower.json

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-'use strict';
-var app = require('express');
-var bodyParser = require('bodyParser');
+var express = require('express');
 var morgan = require('morgan');
 var marked = require('marked');
+var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000;
+var app = express();
 
 app.use(morgan('dev'));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true}));
-
 app.set('view engine', 'jade');
 
+
+//routes
 app.get('/', function(req, res){
   res.render('index');
 });
@@ -20,6 +21,7 @@ app.post('/mdtohtml', function(req, res){
   var html = marked(md);
   res.send(html);
 })
+
 
 app.listen(port);
 console.log('listening on port', port);
